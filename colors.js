738c51d444c642a71082c4901dde362f5c9e8605
Asurite <116214425+Asurite@users.noreply.github.com>
2023-11-11 13:49:52 +0100

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'midnight': '#755CDE',
+        'Summer-Yellow': '#F6A560',
+        'Pink': '#F39E9E',
+        'Light-Red': '#EB7565',
+        'cyan': '#61C4B7',
+        'Purple': '#552049',
+        'Black': '#030303',
+        'Medium-Brown': '#7A746E',
+        'Light-Cream':'#FFF7F0',
+      },
+    },
+  }
